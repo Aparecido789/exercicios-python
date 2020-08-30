d77709a069e6c5c2d3552202850eaf6a4cbccbcd
Aparecido Francisco <aparecidofranciscosilva10@gmail.com>
2020-08-30 16:40:29 -0300

    Otimização e melhoria de código no exercicio2

--- exercicio2.py
+++ exercicio2.py
@@ -3,18 +3,14 @@
 # B) A lista de valores, ordenada de forma decrescente.
 # C) Se o valor 5 foi digitado e está ou não na lista.
 
-pergunta = input('Deseja continuar? S/N     ')
-pergunta.upper()
-valor = int(input('Digite um numero: '))
 lista = []
+while True:
+  lista.append(int(input('Digite um numero: ')))
+  pergunta = input('Deseja continuar? {S/N}  ')
+  if pergunta in 'Nn':
+    break
 
-while pergunta == 'S':
-  lista.append(valor)
-  pergunta = input('Deseja continuar? S/N    ')
-  pergunta.upper()
-  valor = int(input('Digite um numero: '))
-
-print('--------------------------------------------------------')
+print('-' * 30)
 print('Você digitou', len(lista), 'elementos')
 print(sorted(lista, reverse = True))
 if 5 in lista:
